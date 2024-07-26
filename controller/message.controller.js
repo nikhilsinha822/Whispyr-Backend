ae85ffafc92a186be231fef4392b24547ba808ca
nikhilsinha822 <nikhilsinha822@gmail.com>
2024-07-26 23:17:17 +0530

    added the chatList and chatMessage api

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,5 +1,8 @@
 const catchAsyncError = require('../middleware/catchAsyncError.middleware')
 const cloudinary = require('cloudinary').v2
+const Conversation = require('../models/conversation.model')
+const Message = require('../models/message.models')
+const ErrorHandler = require('../utils/errorHandler.util')
 
 const getFileUploadSignature = catchAsyncError(async (req, res, next) => {
     const timestamp = Math.round((new Date).getTime() / 1000);
@@ -9,7 +12,7 @@ const getFileUploadSignature = catchAsyncError(async (req, res, next) => {
         folder: `chatMedia_${req.user._id}`
     }
 
-    const signature = cloudinary.utils.api_sign_request({...cloudinaryOptions}, process.env.CLOUDINARY_API_SECRET);
+    const signature = cloudinary.utils.api_sign_request({ ...cloudinaryOptions }, process.env.CLOUDINARY_API_SECRET);
 
     const payload = {
         ...cloudinaryOptions,
@@ -22,4 +25,55 @@ const getFileUploadSignature = catchAsyncError(async (req, res, next) => {
     res.json({ payload })
 })
 
-module.exports = { getFileUploadSignature }
+const getChatList = catchAsyncError(async (req, res, next) => {
+    const userId = req.user._id;
+
+    const conv = await Conversation.find({
+        participants: {
+            $all: [{ "$elemMatch": { $eq: userId } }]
+        }
+    }).populate('lastMessage').sort({ updatedAt: -1 })
+
+    const convIDs = conv.map((conv) => conv._id)
+    await Promise.all(convIDs.map((convID) => Message.updateMany({
+        conversation: convID,
+        status: { $lt: 1 },
+        sender: { $ne: userId }
+    }, {
+        $set: { status: 1 }
+    })))
+
+    res.status(200).json({
+        success: true,
+        data: conv
+    })
+})
+
+const getChatMessage = catchAsyncError(async (req, res, next) => {
+    const userId = req.user._id;
+    const { conversationId } = req.params;
+
+    const conversation = await Conversation.findById(conversationId);
+    const isParticipant = conversation.participants.includes(userId);
+    if (!isParticipant)
+        return next(new ErrorHandler("Access Denied", 403))
+
+    await Message.updateMany({
+        conversation: conversationId,
+        sender: { $ne: userId }
+    }, {
+        $set: { status: 2 }
+    })
+    const messages = await Message.find({ conversation: conversationId }).sort({ createdAt: -1 });
+
+    res.status(200).json({
+        success: true,
+        data: messages
+    })
+})
+
+module.exports = {
+    getFileUploadSignature,
+    getChatList,
+    getChatMessage
+}
--- routes/message.route.js
+++ routes/message.route.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const router = express.Router();
-const { getFileUploadSignature } = require('../controller/message.controller')
 const verifyJWT = require('../middleware/verifyJWT.middleware')
+const { getFileUploadSignature,
+    getChatList,
+    getChatMessage
+} = require('../controller/message.controller')
 
 router.route('/fileUpload/new')
     .get(verifyJWT, getFileUploadSignature)
+
+router.route('/chat/chatList')
+    .get(verifyJWT, getChatList)
+
+router.route('/chat/chatMessage/:conversationId')
+    .get(verifyJWT, getChatMessage)
+
 
 module.exports = router
